09788862dd05788579d15dd235707630278680c9
Paola Guarasci <paolaguarasci@gmail.com>
2023-04-06 22:53:53 +0200

    init be

---
+++ winted-be/src/main/java/it/pingflood/wintedbe/WintedBeApplication.java
@@ -0,0 +1,13 @@
+package it.pingflood.wintedbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintedBeApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(WintedBeApplication.class, args);
+  }
+  
+}
